241cc0d0afba43080b9052a672937a239f95912a
folderas <marcinwolder7@gmail.com>
2024-07-29 19:19:23 +0200

    recursive method

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import pyzipper
 
+indent = 0
 
-def onTxtFiles(zf):
+def onTxtFile(zf, fileName):
+    global indent
+    test_bytes = zf.read(fileName)
+    print(str(" "*indent),test_bytes)
+
+
+def onZipFile(zf):
+    global indent
+    print(str(" "*indent), f"+{zf.filename}")
+    indent += 4
     for fileName in zf.namelist():
-            if "txt" in fileName:
-                test_bytes = zf.read(fileName)
-                print(test_bytes)
+        if "zip" in fileName:
+            with pyzipper.AESZipFile(fileName) as nestedZf:
+                onZipFile(nestedZf)
+        elif "txt" in fileName:
+            onTxtFile(zf, fileName)
+    indent -= 4
+    print(str(" "*indent), f"-{zf.filename}")
 
 
 def main():
-    tests = ["test-1", "test-2", "test-3", "test-4", "test-5"]
+    tests = ["test-1", "test-2", "test-3", "test-4", "test-5", "test-6"]
     for test in tests:
         print(f"--- {test} ---")
         with pyzipper.AESZipFile(f'{test}.zip') as zf:
-            for fileName in zf.namelist():
-                if "zip" in fileName:
-                    with pyzipper.AESZipFile(zf.open(fileName)) as nestedZf:
-                        onTxtFiles(nestedZf)
-            if not all(["zip" in fileName for fileName in zf.namelist()]):
-                onTxtFiles(zf)
-        
+            onZipFile(zf)
 
 
 if __name__ == "__main__":
